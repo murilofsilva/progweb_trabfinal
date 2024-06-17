48619dc010317d8180765e95d92e1be4d8a281a9
Murilo Fernandes <murilofsilva11@gmail.com>
2024-06-16 20:21:01 -0400

    initial commit

---
+++ progweb/src/main/java/com/ufms/progweb/ProgwebApplication.java
@@ -0,0 +1,13 @@
+package com.ufms.progweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgwebApplication.class, args);
+	}
+
+}
---
+++ progweb/src/test/java/com/ufms/progweb/ProgwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufms.progweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
