54def12574011c49461aeac73bcbddcfb8ed67e7
Murilo Fernandes <murilofsilva11@gmail.com>
2024-06-23 03:05:46 -0400

    Adicionando configurações de banco

--- back/src/main/java/com/ufms/progweb/model/Sneaker.java
+++ back/src/main/java/com/ufms/progweb/model/Sneaker.java
@@ -1,17 +1,32 @@
 package com.ufms.progweb.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
 import java.math.BigDecimal;
 
+@Entity
+@Data
+@Table
 public class Sneaker {
 
+    @Id
+    @Column
     private Long id;
 
+    @Column
     private String nome;
 
+    @Column
     private BigDecimal preco;
 
+    @Column
     private String descricao;
 
+    // Getters e Setters
     public void setId(Long id) {
         this.id = id;
     }
--- back/src/main/java/com/ufms/progweb/model/Usuario.java
+++ back/src/main/java/com/ufms/progweb/model/Usuario.java
@@ -1,15 +1,28 @@
 package com.ufms.progweb.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Usuario {
 
+    @Id
+    @Column
     private Long id;
 
+    @Column
     private String nome;
 
+    @Column
     private String email;
 
+    @Column
     private String senha;
 
+    // Getters e Setters
     public Long getId() {
         return id;
     }
