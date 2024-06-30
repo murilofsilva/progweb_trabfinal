e264b2346a889151c20b86ea8ad25026468d8f4c
Murilo Fernandes <murilofsilva11@gmail.com>
2024-06-30 13:31:58 -0400

    Adiciona configuração de CORS

--- back/src/main/java/com/ufms/progweb/config/SecurityConfig.java
+++ back/src/main/java/com/ufms/progweb/config/SecurityConfig.java
@@ -1,18 +1,12 @@
 package com.ufms.progweb.config;
 
-
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-
 
 @Configuration
 @EnableWebSecurity
@@ -21,15 +15,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/", "/sneakers", "/usuarios", "/{id}", "/filter", "/resources/**", "/static/**", "/images/**", "/icons/**", "/public/**").permitAll()
-                        .anyRequest().authenticated()
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/", "/sneakers", "/usuarios", "/filter", "/resources/**", "/static/**", "/images/**", "/icons/**", "/public/**").permitAll()
+                                .anyRequest().authenticated()
                 )
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .permitAll()
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .permitAll()
                 )
-                .logout(logout -> logout.permitAll());
+                .logout(logout ->
+                        logout.permitAll()
+                );
 
         return http.build();
     }
@@ -38,16 +38,4 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(16);
     }
-
-    private DataSource dataSource;
-
-    protected void userAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.jdbcAuthentication()
-                .dataSource(this.dataSource)
-                .passwordEncoder(passwordEncoder())
-                .usersByUsernameQuery("select username, password, enabled from users where username=?")
-                .authoritiesByUsernameQuery("select username, authority from authorities where username=?"); //ADICIONAR UMA TABELA 'AUTHORITIES' NO BANCO DE DADOS COM O PERMISSIONAMENTO DE CADA TIPO DE USUÁRIO
-    }
-
 }
